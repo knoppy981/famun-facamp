b830237c8148fffa0ce495337bb1799e4babdd18
Andre Knopp <andre.knopp8@gamil.com>
2022-08-15 13:59:21 -0300

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,15 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+
+      <ul>
+        <li>
+          <p>
+            FAMUN
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+}
