a085450eece1e7da429540d6562ddb7b93fd9ae0
Andre Knopp <andre.knopp8@gamil.com>
2022-08-22 21:55:13 -0300

    login 1

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,20 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { requireUserId } from "~/session.server";
+
+export const loader = async ({ request, params }) => {
+	const userId = await requireUserId(request);
+	if (!userId) {
+		throw new Response("User not found", { status: 404 });
+	}
+	return json({ userId });
+};
+
 export default function Index() {
+
+	const data = useLoaderData()
+
   return (
     <div>
       <h1>Welcome to Remix</h1>
