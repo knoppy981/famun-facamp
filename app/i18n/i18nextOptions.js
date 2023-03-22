294bf8d9d9c64e628243afcd2e9731fb488bcd78
Andre Knopp <andre.knopp8@gamil.com>
2023-03-22 14:23:46 -0300

    update fallbacklng

--- app/i18n/i18nextOptions.js
+++ app/i18n/i18nextOptions.js
@@ -1,6 +1,6 @@
 export default {
   debug: /* process.env.NODE_ENV !== 'production' */ false,
-  fallbackLng: 'en',
+  fallbackLng: 'en-US',
   supportedLngs: ['en-US', 'pt-BR', 'es-ES'],
   react: { useSuspense: false }
 }
